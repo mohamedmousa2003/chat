648a2d464afa7209acb43526cd13b02118d8fbfa
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-05-08 17:50:46 +0300

    ADD login

---
+++ lib/Shared/styles/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color white = Colors.white;
+
+Color black = Colors.black;
+
+Color grey = Colors.grey;
+
+Color blue = const Color(0xFF11DCE8);
---
+++ lib/pages/login/widget/custom_buttom.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../../../Shared/styles/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({super.key, required this.onTap, required this.title});
+
+  void Function()? onTap;
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: white, borderRadius: BorderRadius.circular(10)),
+        child: Center(
+          child: Text(
+            title,
+            style: theme.textTheme.bodyLarge?.copyWith(color: black),
+          ),
+        ),
+      ),
+    );
+  }
+}
